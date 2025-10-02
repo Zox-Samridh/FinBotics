0e820a7d0288707caba30112ec5678c81e391e7e
Sam B <sam@local.dev>
2025-10-02 17:40:37 +0530

    Preparing for deployment

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -2,12 +2,12 @@ import { Suspense } from "react";
 import { getUserAccounts } from "@/actions/dashboard";
 import { getDashboardData } from "@/actions/dashboard";
 import { getCurrentBudget } from "@/actions/budget";
-import { AccountCard } from "./_components/account-card";
+import { AccountCard } from "@/app/(main)/dashboard/_components/account-card";
 import { CreateAccountDrawer } from "@/components/create-account-drawer";
-import { BudgetProgress } from "./_components/budget-progress";
+import { BudgetProgress } from "@/app/(main)/dashboard/_components/budget-progress";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
-import { DashboardOverview } from "./_components/transaction-overview";
+import { DashboardOverview } from "@/app/(main)/dashboard/_components/transactions-overview";
 
 export default async function DashboardPage() {
   const [accounts, transactions] = await Promise.all([
