0f23ac509f23d4e7ebe764b00ab5cd855beeb3fd
Sam B <sam@local.dev>
2025-10-02 17:04:43 +0530

    Preparing for deployment

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";  // 🚀 Quick Fix
+
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import { motion } from "framer-motion";
